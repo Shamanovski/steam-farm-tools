41a024e70a3819366d0abe82bc0bdbdfdb304fe2
Shamanovski <spike2@list.ru>
2017-10-03 14:55:38 +0300

    add logging, use Firefox webdriver to get steamkeys catalogue, fetch market_links

--- server/parse_catalogue.py
+++ server/parse_catalogue.py
@@ -1,27 +1,29 @@
 import re
 import logging
 import json
-import collections
 import sys
-import io
-import urllib3
+import logging
+import traceback
 
-import requests
+from pyvirtualdisplay import Display
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO,
+                    filename='parse_catalogue_log.txt', filemode='w')
 
-sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
-sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+def uncaught_exceptions_handler(type, value, tb):
+    logging.critical("{0} {1}\n{2}".format(type, value, traceback.format_tb(tb)))
+    sys.exit(1)
 
+sys.excepthook = uncaught_exceptions_handler
 
 def main():
-    driver = webdriver.PhantomJS('server/phantomjs', service_log_path='phantomjs.log')
-    driver.get('http://steamkeys.ovh/?key=7f35e0db30daeb9c4dc813b641f7f0cf')
-    html = driver.page_source
-    driver.quit()
-    with open('bla.html', 'w', encoding='utf-8') as f:
-        f.write(html)
+    with Display():
+        driver = webdriver.Firefox()
+        driver.get('http://steamkeys.ovh/?key=7f35e0db30daeb9c4dc813b641f7f0cf')
+        html = driver.page_source
+        driver.quit()
     soup = BeautifulSoup(html, "html.parser")
     catalogue_items = get_catalogue(soup)
 
@@ -34,6 +36,7 @@ def get_catalogue(soup_obj):
     catalogue_items = {}
     for row in rows[1:]:
         tds = row.find_all("td")
+        market_link = row.td.a['href'].replace('price_desc', 'price_asc')
         appid = re.search(r'.+/app/(\d+)/', tds[1].a['href']).group(1)
         game_name = tds[1].text
         price = float(tds[2].a.text)
@@ -45,13 +48,21 @@ def get_catalogue(soup_obj):
         try:
             lequeshop_id = re.search(r'type=(\d+)\&', tds[2].find(class_='qckp')['href']).group(1)
         except (AttributeError, TypeError) as err:
-            print(err)
+            logging.error("Couldn't identify lequeshop product %s", tds[2])
             lequeshop_id = None
         try:
             amount = int(amount)
         except ValueError:
             pass
-        catalogue_items[appid] = {'store': store, 'price': price, 'amount': amount, 'game_name': game_name, 'lequeshop_id': lequeshop_id}
+
+        catalogue_items[appid] = {
+            'store': store,
+            'price': price,
+            'amount': amount,
+            'game_name': game_name,
+            'lequeshop_id': lequeshop_id,
+            'market_link': market_link
+        }
     return catalogue_items
 
 
