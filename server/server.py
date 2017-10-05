3be41a033599b6031ef75f959a4598cd9dd6e962
Shamanovski <spike2@list.ru>
2017-10-05 18:44:52 +0300

    refactor logging

--- server/server.py
+++ server/server.py
@@ -2,6 +2,7 @@ import json
 import logging
 import shelve
 import requests
+import os
 
 from flask import Flask, request
 
@@ -11,7 +12,7 @@ logging.getLogger("requests").setLevel(logging.ERROR)
 
 logger = logging.getLogger()
 logger.setLevel(level=logging.INFO)
-file_handler = logging.FileHandler('logs.txt', 'a', encoding='utf-8')
+file_handler = logging.FileHandler('my_server/logs.txt', 'a', encoding='utf-8')
 formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
@@ -26,7 +27,7 @@ def check_license_autoreg():
     with open('farmtools_keys.txt', 'r') as f:
         farmtools_keys = [i.rstrip() for i in f.readlines()]
 
-    success = check_license(autoreg_keys + farmtools_keys)
+    success = check_license(autoreg_keys + farmtools_keys, 'autoreg')
     return json.dumps({'success': success}), 200
 
 
@@ -35,11 +36,12 @@ def check_license_farmtools():
     with open('farmtools_keys.txt', 'r') as f:
         farmtools_keys = [i.rstrip() for i in f.readlines()]
 
-    success = check_license(farmtools_keys)
+    success = check_license(farmtools_keys, 'farmtools')
     return json.dumps({'success': success}), 200
 
 
-def check_license(keys):
+def check_license(keys, app_name):
+    logging.info('Application: %s', app_name)
     success = False
     data = {key: value for key, value in request.form.items()}
     ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
@@ -56,7 +58,7 @@ def check_license(keys):
                 db_data = db[key]
                 success = check_device(data, db_data, ip)
         else:
-            logger.info('WRONG KEY: %s, %s', data, ip)
+            logger.info('WRONG KEY: %s, %s\n', data, ip)
     finally:
         db.close()
 
@@ -94,12 +96,12 @@ def get_city_from_ip(ip_address):
 
 def update_database(data, db, key):
     db[key] = data
-    logger.info('VALID KEY. Added to the database: %s', data)
+    logger.info('VALID KEY. Added to the database: %s\n', data)
 
 
 def check_device(data, db_data, ip):
     if data['uid'] != db_data['uid']:
-        logger.warning('UID is different (%s). The request has been declined: %s', data['uid'], db_data)
+        logger.warning('UID is different (%s). The request has been declined: %s\n', data['uid'], db_data)
         return False
 
     stored_ip, stored_city = db_data['ip']
@@ -110,5 +112,5 @@ def check_device(data, db_data, ip):
                 'Data from database: %s', ip, city, db_data)
         logger.warning('IPs are different: %s-%s', ip, stored_ip)
 
-    logger.info('The device has been authorized successfully: %s', db_data)
+    logger.info('The device has been authorized successfully: %s\n', db_data)
     return True
